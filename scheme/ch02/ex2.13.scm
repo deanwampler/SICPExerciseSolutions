cdb68117815f7dd0a58da8eb500059bb9cac9728
Dean Wampler <dean@polyglotprogramming.com>
2009-10-14 20:05:37 -0500

    Removed some unused cut-n-paste code.

--- scheme/ch02/ex2.13.scm
+++ scheme/ch02/ex2.13.scm
@@ -16,13 +16,6 @@
   (let ((half-delta (/ (- (upper-bound i) (lower-bound i)) 2.0)))
     (* (/ half-delta (+ (lower-bound i) half-delta)) 100.0)))
   
-(define i (make-center-percent 100.0 1.0))
-
-(check-equal? (lower-bound i)  99.0)
-(check-equal? (upper-bound i) 101.0)
-(check-equal? (center i)      100.0)
-(check-equal? (percent i)       1.0)
-
 ; mul-interval definition from ex2.11, if you assume that no negative lower and
 ; and upper bounds are ever negative.
 (define (mul-interval x y)
