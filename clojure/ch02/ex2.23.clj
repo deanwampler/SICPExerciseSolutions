7da3d1cebaa70d6ca1a0aa8a263e37f673a7def0
Dean Wampler <dean@polyglotprogramming.com>
2009-11-03 15:21:42 -0600

    Bug fix for NPE.

--- clojure/ch02/ex2.23.clj
+++ clojure/ch02/ex2.23.clj
@@ -2,7 +2,6 @@
 
 (defn for-each [f items]
   (if (not (empty? items))
-      ((f (first items)) (for-each f (rest items)))))
+      (let [] (f (first items)) (for-each f (rest items)))))
 
 (for-each (fn [x] (println (* x x))) (list 1 2 3 4 5))
-  
