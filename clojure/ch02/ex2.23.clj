1305e120e0344d0decb24759f05387caca54b7ce
Dean Wampler <dean@polyglotprogramming.com>
2009-11-03 15:31:19 -0600

    Better, iterative implementation.

--- clojure/ch02/ex2.23.clj
+++ clojure/ch02/ex2.23.clj
@@ -1,7 +1,10 @@
 (ns sicp.ch02 (:use clojure.contrib.test-is))
 
+; Version suggested on the clojure mailing list by Brian Hurt.
 (defn for-each [f items]
-  (if (not (empty? items))
-      (let [] (f (first items)) (for-each f (rest items)))))
+  (loop [curr items]
+    (if (empty? curr)
+      nil
+      (do (f (first curr)) (recur (rest curr))))))
 
 (for-each (fn [x] (println (* x x))) (list 1 2 3 4 5))
