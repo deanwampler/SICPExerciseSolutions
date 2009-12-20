3de8609528d58b465f31b11cc08916c052cc2d7c
Dean Wampler <dean@polyglotprogramming.com>
2009-12-20 16:01:04 -0600

    Clojure solutions now use the built-in unit testing library, rather than the deprecated version in contrib.

--- clojure/run.sh
+++ clojure/run.sh
@@ -2,7 +2,5 @@
 # Simple script to run a single exercise
 # Set these variables in your shell or use the same values you used in the Makefile.
 # CLOJURE_HOME =
-# CLOJURE_CONTRIB_HOME =
 
-#java -cp $CLOJURE_HOME/clojure.jar:$CLOJURE_CONTRIB_HOME/clojure-contrib.jar clojure.main "$@"
 java -cp $CLOJURE_HOME/clojure.jar clojure.main "$@"
